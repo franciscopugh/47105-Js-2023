3a4aced030b1fc5cfc91207e69b7b434f5575161
Francisco Pugh <franciscopugh01@gmail.com>
2023-09-28 06:10:54 -0300

    Clase 1

---
+++ clase-1/main.js
@@ -0,0 +1,52 @@
+// Esto es un comentario de una linea
+/*
+ Este es un comentario multi-linea
+ */
+
+//= es asignacion. Cuando asigno aplico valor y tipo de dato
+/*
+//String
+var nombre = "Francisco";
+var apellido = 'Pugh';
+var email = `f@f.com`;
+
+//Number
+
+var edad = 40;
+var sueldo = 100.5;
+
+//Boolean o logicos
+var alumno = false;
+var profesor = true;
+
+// Undefined osea no definido
+//NaN = Not a Number, No es un numero. NaN no es igual a undefined
+//Null significa vacio que no es lo mismo que undefined
+var casaPropia
+var bebidaEnCasa = null
+
+//Es para mostrar informacion en el navegador
+nombre = 4
+console.log(nombre)
+nombre = true
+console.log(nombre)
+nombre = undefined
+console.log(nombre)
+
+
+let direccion = "Calle Falsa 123" //Por defecto estoy haciendo esto -> var direccion
+direccion = "Calle verdadera 456"
+
+const IVA = 1.21
+const URL_PRESTAMOS = "http://prestamosya.com"
+console.log(direccion)
+console.log(IVA)
+console.log(URL_PRESTAMOS)*/
+
+let numero1 = 5
+let numero2 = '6'
+
+console.log(numero1 * numero2) //30 numerico
+console.log(numero1 + numero2) //30 numerico
+console.log(1000 + 2000 + 1500 + 0)
+
